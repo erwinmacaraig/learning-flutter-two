6c6309669eabafe9160480d0abe6bc2a53475dca
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-03-18 08:26:16 +0800

    Adding Material Icons with the Icon Widget

--- lib/main.dart
+++ lib/main.dart
@@ -13,22 +13,77 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            crossAxisAlignment: CrossAxisAlignment.center,
+          child: Column(
             children: [
-              Container(
-                color: Colors.red,
-                width: 100.0,
+              const CircleAvatar(
+                radius: 50.0,
+                backgroundColor: Colors.red,
+                backgroundImage: AssetImage('images/id.jpg'),
+              ),
+              const Text(
+                'Erwin Macaraig',
+                style: TextStyle(
+                    fontFamily: 'Pacifico',
+                    fontSize: 40.0,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold),
+              ),
+              Text(
+                'Full Stack Developer',
+                style: TextStyle(
+                    fontFamily: 'SourceSansPro',
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.teal.shade100,
+                    letterSpacing: 2.5),
               ),
               Container(
-                color: Colors.yellow,
-                height: 100.0,
-                width: 100.0,
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    const Icon(
+                      Icons.phone,
+                      color: Colors.teal,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+925 4562 1478',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                  ],
+                ),
               ),
               Container(
-                color: Colors.blue,
-                width: 100.0,
+                color: Colors.white,
+                margin: const EdgeInsets.symmetric(
+                    vertical: 10.0, horizontal: 25.0),
+                padding: EdgeInsets.all(10.0),
+                child: Row(
+                  children: [
+                    Icon(
+                      Icons.email,
+                      color: Colors.teal.shade900,
+                    ),
+                    const SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'emacaraig@kindercaring.com.au',
+                      style: TextStyle(
+                          fontSize: 20.0,
+                          color: Colors.teal.shade900,
+                          fontFamily: 'SourceSansPro'),
+                    ),
+                  ],
+                ),
               ),
             ],
           ),
