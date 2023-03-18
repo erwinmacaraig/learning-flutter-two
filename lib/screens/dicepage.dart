19bd5a9efa178f3af98fad90cabd12ddff983323
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-03-18 12:47:03 +0800

    more on Padding widget

--- lib/screens/dicepage.dart
+++ lib/screens/dicepage.dart
@@ -9,20 +9,28 @@ class DicePage extends StatelessWidget {
         title: Text('Dicee'),
         backgroundColor: Colors.red,
       ),
-      body: Row(
-        children: [
-          Expanded(
-            flex: 1, //for learning
-            child: Image.asset('images/dice1.png'),
-          ),
-          Expanded(
-            flex: 1,
-            child: Image(
-              image: AssetImage(
-                  'images/dice2.png'), // purposely left this behind for learning and comparison with the Image Provider
+      body: Center(
+        child: Row(
+          children: [
+            Expanded(
+              flex: 1, //for learning
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Image.asset('images/dice1.png'),
+              ),
             ),
-          ),
-        ],
+            const Expanded(
+              flex: 1,
+              child: Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Image(
+                  image: AssetImage(
+                      'images/dice2.png'), // purposely left this behind for learning and comparison with the Image Provider
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
