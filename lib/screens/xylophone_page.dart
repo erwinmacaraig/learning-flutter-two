05c041e8f01d1f2ef443e018b5fdc65e69878dbf
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-03-28 18:50:14 +0800

    function with named parameters

--- lib/screens/xylophone_page.dart
+++ lib/screens/xylophone_page.dart
@@ -4,24 +4,35 @@ import 'dart:async';
 import 'package:audioplayers/audioplayers.dart';
 
 class Xylophone extends StatelessWidget {
+  final player = AudioPlayer();
+
+  Widget buildContainer({required Color color, required int soundNumber}) {
+    return Container(
+      color: color,
+      width: double.infinity,
+      child: TextButton(
+        child: Text('$soundNumber'),
+        onPressed: () async {
+          await player.play(AssetSource('assets_note$soundNumber.wav'));
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Colors.black,
       body: SafeArea(
-        child: Center(
-          child: TextButton(
-            onPressed: () async {
-              final player = AudioPlayer();
-              // await player.setSource(
-              //   AssetSource('assets_note1.wav'),
-              // );
-              await player.play(AssetSource('assets_note1.wav'));
-              // await player.play(
-              //   DeviceFileSource('assets_note1.wav'),
-              // );
-            },
-            child: Text('Click Here!'),
-          ),
+        child: Column(
+          children: [
+            buildContainer(color: Colors.red, soundNumber: 1),
+            buildContainer(color: Colors.orange, soundNumber: 2),
+            buildContainer(color: Colors.yellow, soundNumber: 3),
+            buildContainer(color: Colors.teal, soundNumber: 4),
+            buildContainer(color: Colors.green, soundNumber: 5),
+            buildContainer(color: Colors.purple, soundNumber: 6),
+          ],
         ),
       ),
     );
