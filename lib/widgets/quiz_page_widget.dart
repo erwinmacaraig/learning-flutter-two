d94de89f90610435f575ef3ef10671f0fd6ee331
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-03-29 15:14:23 +0800

    question class

--- lib/widgets/quiz_page_widget.dart
+++ lib/widgets/quiz_page_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../models/models.dart';
 
 class QuizPage extends StatefulWidget {
   @override
@@ -6,41 +7,28 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
-  List<Widget> scoreKeeper = <Widget>[
-    const Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    const Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
-    const Icon(
-      Icons.close,
-      color: Colors.red,
-    ),
-    const Icon(
-      Icons.check,
-      color: Colors.green,
-    ),
+  List<Widget> scoreKeeper = <Widget>[];
+  List<Question> questions = [
+    Question(q: 'You can lead a cow down stairs but not up stairs', a: false),
+    Question(
+        a: true, q: 'Approximately one quarter of human bones are in the feet'),
+    Question(q: 'A slug\'s blood is green', a: true)
   ];
+
+  int questionIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Column(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
       crossAxisAlignment: CrossAxisAlignment.stretch,
       children: [
-        const Expanded(
+        Expanded(
           flex: 5,
           child: Padding(
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'Question Placeholder',
+                questions[questionIndex].questionText,
                 textAlign: TextAlign.center,
                 style: TextStyle(fontSize: 25.0, color: Colors.white),
               ),
@@ -53,9 +41,12 @@ class _QuizPageState extends State<QuizPage> {
             child: TextButton(
               style: TextButton.styleFrom(backgroundColor: Colors.green),
               onPressed: () {
+                bool correctAnswer = questions[questionIndex].questionAnswer;
+                if (correctAnswer == true) {}
                 setState(() {
+                  questionIndex++;
                   scoreKeeper.add(
-                    Icon(
+                    const Icon(
                       Icons.check,
                       color: Colors.green,
                     ),
@@ -77,7 +68,19 @@ class _QuizPageState extends State<QuizPage> {
             padding: const EdgeInsets.all(15.0),
             child: TextButton(
               style: TextButton.styleFrom(backgroundColor: Colors.red),
-              onPressed: () {},
+              onPressed: () {
+                bool correctAnswer = questions[questionIndex].questionAnswer;
+                if (correctAnswer == false) {}
+                setState(() {
+                  questionIndex++;
+                  scoreKeeper.add(
+                    const Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ),
+                  );
+                });
+              },
               child: const Text(
                 'False',
                 style: TextStyle(fontSize: 20.0, color: Colors.white),
