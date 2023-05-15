a7a5ffeecacd5d9eb32298e4b26ac5da2ffe71ec
Erwin Macaraig <erwin.macaraig@gmail.com>
2023-05-15 08:25:49 +0800

    initial work on http api calls

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,11 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:geolocator/geolocator.dart';
+import '../services/location.dart';
+import 'package:http/http.dart' as http;
+
+const appKey = '0930ce3cdef97ec851091466f5de982e';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -7,6 +13,15 @@ class LoadingScreen extends StatefulWidget {
 }
 
 class _LoadingScreenState extends State<LoadingScreen> {
+  late double latitude;
+  late double longitude;
+
+  @override
+  void initState() {
+    super.initState();
+    getLocation();
+  }
+
   void getLocation() async {
     bool serviceEnabled;
     LocationPermission permission;
@@ -32,9 +47,46 @@ class _LoadingScreenState extends State<LoadingScreen> {
           'Location permissions are permanently denied, we cannot request permission');
     }
 
-    Position position = await Geolocator.getCurrentPosition(
-        desiredAccuracy: LocationAccuracy.low);
-    print(position);
+    Location location = Location();
+    await location.getCurrentLocation();
+    latitude = location.latitude;
+    longitude = location.longitude;
+    getData();
+    // print(location.latitude);
+    // print(location.longitude);
+    // try {
+    //   Position position = await Geolocator.getCurrentPosition(
+    //       desiredAccuracy: LocationAccuracy.low);
+    //   print(position);
+    // } catch (e) {
+    //   print(e);
+    // }
+  }
+
+  void getData() async {
+    final url = Uri.https('api.openweathermap.org', '/data/2.5/weather', {
+      'lat': latitude.toString(),
+      'lon': longitude.toString(),
+      'appid': appKey
+    });
+
+    final http.Response response = await http.get(url);
+    // print(response.body);
+    if (response.statusCode == 200) {
+      String data = response.body;
+      var decodedData = jsonDecode(data);
+      double temperature = decodedData['main']['temp'];
+      int condition = decodedData['weather'][0]['id'];
+      String cityName = decodedData['name'];
+
+      print(temperature);
+      print(condition);
+      print(cityName);
+      // print(decodedData);
+
+      // var longitude = decodedData['coord']['lon'];
+      // print(longitude);
+    }
   }
 
   @override
@@ -48,7 +100,7 @@ class _LoadingScreenState extends State<LoadingScreen> {
         child: ElevatedButton(
           child: Text('Get Location'),
           onPressed: () {
-            getLocation();
+            // getLocation();
           },
         ),
       ),
